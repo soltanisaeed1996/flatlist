8745f5194e9aebd6a043823908a8fe09e6f95646
Saeed <soltanisaeed1996@gmail.com>
2020-10-08 19:21:14 +0330

    add header to flatlist

--- App.js
+++ App.js
@@ -7,14 +7,26 @@
  */
 
 import React from 'react'
-import { View, Text, FlatList, StyleSheet } from 'react-native'
-const items = [{ id: '0', text: 'View' },
-{ id: '1', text: 'Text' },
-{ id: '2', text: 'Image' },
-{ id: '3', text: 'ScrollView' },
-{ id: '4', text: 'ListView' },]
+import { View, Text, FlatList, StyleSheet, Alert } from 'react-native'
+const items = [{ key: '0', text: 'View' },
+{ key: '1', text: 'Text' },
+{ key: '2', text: 'Image' },
+{ key: '3', text: 'ScrollView' },
+{ key: '4', text: 'ListView' },]
 
-
+const itemSeparator = () => {
+  return <View style={styles.separator} />
+}
+const getItemList = (item) => {
+  Alert.alert(item)
+}
+const headerList = () => {
+  return (
+    <View style={styles.headerView}>
+      <Text style={styles.headerText}>header </Text>
+    </View>
+  )
+}
 const App = () => {
   return (
 
@@ -25,15 +37,20 @@ const App = () => {
     //     console.log(fff)
     //     return(fff.id)} }/>
     <FlatList
-      style={styles.container}
+      contentContainerStyle={styles.container}
+      style={styles.flatList}
       data={items}
-      //   renderItem={({ item }) => <Text style={styles.row}>{item.text}</Text>}
-      renderItem={({ item }) => <Text style={styles.row}>{item.text}</Text>}
+      ItemSeparatorComponent={itemSeparator}
+      ListHeaderComponent={headerList}
 
-      //keyExtractor={(item) => item.id}
-      // keyExtractor={(item) => item.id}
-        
-     
+      //   renderItem={({ item }) => <Text style={styles.row}>{item.text}</Text>}
+      renderItem={({ item }) => <Text style={styles.row}
+      onPress={getItemList.bind(this, item.key)}>{item.text}</Text>}
+
+    //keyExtractor={(item) => item.id}
+    // keyExtractor={(item) => item.id}
+
+
     />
 
   )
@@ -50,14 +67,30 @@ const App = () => {
 const styles = StyleSheet.create(
   {
     container: {
-      flex: 1
-    },
-    flatList: {
-      flex: 1
-    },
-    row: {
+      flex: 1,
+      padding: 10,
 
     },
+    flatList: {
+      // flex: 1,
+    },
+    row: {
+      marginBottom: 5,
+      marginTop: 5,
+      backgroundColor: 'skyblue',
+      paddingLeft: 5,
+      paddingTop: 5,
+      paddingBottom: 5,
+      flexDirection: 'row',
+      fontSize: 12
+    },
+    separator: {
+      height: 1,
+      width: '100%',
+      backgroundColor: 'gray'
+    },
+    headerView: {backgroundColor: 'green',padding:5},
+    headerText: { fontSize: 18, color: 'red',textAlign:'center'  }
   }
 )
 
